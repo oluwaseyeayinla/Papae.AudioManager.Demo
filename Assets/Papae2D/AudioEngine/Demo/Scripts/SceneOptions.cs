39c187c930f0c3e8f3e20024c35852cf0bcc428d
oluwaseyeayinla <oluwaseyeayinla@gmail.com>
2017-01-04 12:29:52 +0100

    Upload to bitbucket

---
+++ Assets/Papae2D/AudioEngine/Demo/Scripts/SceneOptions.cs
@@ -0,0 +1,137 @@
+﻿using Papae2D.AudioEngine;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SceneOptions : MonoBehaviour
+{
+    // reference to the black screen animator component.
+    public Animator blackscreenAnimator;
+    // fade to transparent animation clip
+    public AnimationClip fadeToTransparentAnimationClip;
+
+    public bool IsInMainMenu
+    {
+        get { return SceneManager.GetActiveScene().name == "MenuScene"; }
+    }
+
+    // reference to the PanelOptions script
+    PanelOptions panelOptions;                  
+
+    
+    void Awake()
+	{
+        // retrieve the attached PanelOptions script
+        panelOptions = GetComponent<PanelOptions> ();
+	}
+
+    void Start()
+	{
+        // fade the black screen to transparent
+        blackscreenAnimator.SetTrigger("toTransparent");
+        // play right background music if none is currently active
+		PlayMusic ();
+	}
+
+    public void PlayMusic()
+    {
+        AudioClip clip = null;
+
+        // load the scenes by name to decide which music clip to play.
+        switch (SceneManager.GetActiveScene().name)
+        {
+            // load the title music from the resources folder using AudioManager
+            case "MenuScene":
+                clip = AudioManager.LoadClipFromResources("MenuMusic");
+                break;
+            // load the game music from the resources folder using AudioManager
+            case "GameScene":
+                clip = AudioManager.LoadClipFromResources("GameMusic");
+                break;
+        }
+
+        // play the background music using the fade transition from the assigned clip
+        if (clip)
+        {
+            AudioManager.PlayBGM(clip, MusicTransition.LinearFade);
+        }
+    }
+
+    // function used to load or exit gameplay
+    public void StartGame(bool flag)
+    {
+        if (flag)
+            AudioManager.PlayOneShotFromResources("Button", LoadGame);
+        else
+            AudioManager.PlayOneShotFromResources("Button", LoadMainMenu);
+    }
+
+    void LoadGame()
+    {
+        // fade out current music and fade in next music in 1s
+        AudioManager.PlayBGMFromResources("GameMusic", MusicTransition.LinearFade);
+        
+        // disable interaction with the main menu UI
+        panelOptions.DisableMainMenu();
+
+        // delay calling of LoadGameScene by half the length of fadeColorAnimationClip
+        Invoke("LoadGameScene", fadeToTransparentAnimationClip.length * 1);
+
+        // trigger the transparent Animator to start transition to the FadeToOpaque state.
+        blackscreenAnimator.SetTrigger("toOpaque");
+    }
+
+    void LoadGameScene()
+	{
+		SceneManager.LoadScene("GameScene");
+	}
+
+    void LoadMainMenu()
+    {
+        // fade out current music and fade in next music in 1s
+        AudioManager.PlayBGMFromResources("MenuMusic", MusicTransition.LinearFade);
+
+        // disable interaction with the pause menu UI
+        panelOptions.DisablePauseMenu();
+
+        // delay calling of LoadMenuScene by half the length of fadeColorAnimationClip
+        Invoke("LoadMenuScene", fadeToTransparentAnimationClip.length * 1);
+
+        // trigger of Animator 'animColorFade' to start transition to the FadeToOpaque state.
+        blackscreenAnimator.SetTrigger("toOpaque");
+    }
+
+	void LoadMenuScene()
+	{
+        SceneManager.LoadScene("MenuScene");
+    }
+
+    // display on or off the options menu after the button sound has been played
+    public void DisplayOptionsMenu(bool flag)
+    {
+        if (flag)
+            AudioManager.PlayOneShotFromResources("Button", panelOptions.ShowOptionsMenu);
+        else
+            AudioManager.PlayOneShotFromResources("Button", panelOptions.HideOptionsMenu);
+    }
+
+    public void QuitGame()
+    {
+        AudioManager.PlayOneShotFromResources("Button", Exit);
+    }
+
+    void Exit()
+    {
+        //If we are running in a standalone build of the game
+        #if UNITY_STANDALONE
+        //Quit the application
+        Application.Quit();
+        #endif
+
+        //If we are running in the editor
+        #if UNITY_EDITOR
+        //Stop playing the scene
+        UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+    }
+}
---
+++ Docs/Promotional Images Redesign/README.txt
@@ -0,0 +1,29 @@
+I Yinka. Once again compliments of the season.
+
+You could totally scrap what I have in mind and go with what you think. But you would would need to produce a better 
+version of the current one all in their 3 different sizes. Would need it by Wednesday latest. So no rush. Lol!
+
+# Product description
+The Audio Manager is a library plugin that provides a simple way to manage and control your 2D game’s background music 
+and sound effects.
+
+# What I have in mind
+For now all I know is that I would want
+A controller icon of some sort in it (Not a game controller)
+A Music icon and maybe a sound effects icon to represent sound
+Or they could be in form of a wave
+
+
+# Reason
+The reason why you are redesigning the images are stated below:
+
+It was said that my previous key images are not to the quality that is an acceptable standard for the Asset Store. 
+Quite often this takes the form of an image being too busy, clashing colors, using the Unity camera default blue 
+or default skybox, plain text, or not being a good representation of your content.
+Please redesign your images to be more aesthetically pleasing and attractive. 
+Having high-quality key images improves your packages marketing presence and is quite a substantial portion 
+of being successful on the Asset Store
+
+
+# Guidelines
+There are a few guidelines you would need to adhere to. I've attached a .pdf document of the guidelines
